7fa1f1dc8965d928d14d279a47d4fc7500f311e9
Tu Kim Hoang <tukimhoang@MacBook-Pro.local>
2024-09-27 16:35:38 +0700

    update html and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        main: "linear-gradient(270.78deg, #c3dafe 4.19%, #e9d8fd 96.11%)"
+      }
+    },
+  },
+  
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+}
